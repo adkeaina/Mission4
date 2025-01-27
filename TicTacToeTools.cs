517b0300218530cf8c794c69c54c56d1c8064620
adkeaina <76181539+adkeaina@users.noreply.github.com>
2025-01-27 13:22:49 -0700

    IsWinner almost done

--- TicTacToeTools.cs
+++ TicTacToeTools.cs
@@ -34,4 +34,39 @@ public class TicTacToeTools
             Console.WriteLine();
         }
     }
+
+    public int IsWinner(char[,] board)
+    {
+        // check rows for winner
+        for (int row = 0; row < board.GetLength(0); row++)
+        {
+            int xCount = 0;
+            int oCount = 0;
+            for (int col = 0; col < board.GetLength(1); col++)
+            {
+                if (board[row, col] == 'X') xCount++;
+                else if (board[row, col] == 'O') oCount++;
+            }
+
+            if (xCount == 3) return 1;
+            else if (oCount == 3) return 2;
+        }
+        
+        //check cols for winner
+        for (int col = 0; col < board.GetLength(1); col++)
+        {
+            int xCount = 0;
+            int oCount = 0;
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                if (board[row, col] == 'X') xCount++;
+                else if (board[row, col] == 'O') oCount++;
+            }
+            if (xCount == 3) return 1;
+            else if (oCount == 3) return 2;
+        }
+        
+        // check diagonals for winner
+        return 0;
+    }
 }
