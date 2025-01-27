af4bc03ac257613a5b7e50f589db038092a1b24a
adkeaina <76181539+adkeaina@users.noreply.github.com>
2025-01-27 13:12:11 -0700

    s

--- TicTacToeTools.cs
+++ TicTacToeTools.cs
@@ -2,5 +2,36 @@ namespace Mission4;
 
 public class TicTacToeTools
 {
-    
+
+    public void PrintBoard(char[,] board)
+    {
+        for (int row = 0; row < board.GetLength(0); row++)
+        {
+            if (row != 0)
+            {
+                Console.WriteLine("------------");
+            }
+            for (int col = 0; col < board.GetLength(1); col++)
+            {
+                if (col != 0)
+                {
+                     Console.Write($" | ");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+                if (board[row, col] == '\0')
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write($"{board[row, col]} ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
 }
