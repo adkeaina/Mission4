8149463b3eee36f22bcb863874cd4c7bbd0ab8b3
nicktimbellini <nicholas.bellini@live.com>
2025-01-27 13:50:15 -0700

    added play again feature

--- Program.cs
+++ Program.cs
@@ -22,61 +22,79 @@ int player = 1;
 int row;
 int col;
 bool endgame = true;
-
+bool playagain = true;
+string response = "";
 // while endgame is true, the game plays
-while (endgame)
+do
 {
-    t4.PrintBoard(gameBoard);
-
-    Console.Write($"Player {player} pick where you want to go (1-9):");
-    string input = Console.ReadLine();
-
-    int play = int.Parse(input);
-
-    row = (play - 1) / 3;
-    col = (play - 1) % 3;
-    // Check if the chosen spot is already taken
-    if (gameBoard[row, col] == 'X' || gameBoard[row, col] == 'O')
+    while (endgame)
     {
-        Console.WriteLine("Spot already taken. Choose another one.");
-        continue;
+        t4.PrintBoard(gameBoard);
+        // 
+        Console.Write($"Player {player} pick where you want to go (1-9):");
+        string input = Console.ReadLine();
+
+        int play = int.Parse(input);
+
+        row = (play - 1) / 3;
+        col = (play - 1) % 3;
+        // Check if the chosen spot is already taken
+        if (gameBoard[row, col] == 'X' || gameBoard[row, col] == 'O')
+        {
+            Console.WriteLine("Spot already taken. Choose another one.");
+            continue;
+        }
+
+        if (player == 1)
+        {
+            gameBoard[row, col] = 'X';
+        }
+        else
+        {
+            gameBoard[row, col] = 'O';
+        }
+
+        if (player == 1)
+        {
+            player = 2;
+        }
+        else
+        {
+            player = 1;
+        }
+        int winner = t4.IsWinner(gameBoard);
+        if (winner == 1)
+        {
+            Console.WriteLine("Player 1 won!");
+            endgame = false;
+        }
+        else if (winner == 2)
+        {
+            Console.WriteLine("Player 2 won!");
+            endgame = false;
+        }
+        else if (winner == -1)
+        {
+            Console.WriteLine("CATS GAME!");
+            endgame = false;
+        }
+        else { continue; }
+
     }
 
-    if (player == 1)
+    Console.WriteLine("Do you want to play again? (Yes/No)");
+    response = Console.ReadLine();
+    response = response.ToLower();
+    if (response == "no")
     {
-        gameBoard[row, col] = 'X';
+        playagain = false;
     }
     else
     {
-        gameBoard[row, col] = 'O';
+        playagain = true;
     }
 
-    if (player == 1)
-    {
-        player = 2;
-    }
-    else
-    {
-        player = 1;
-    }
-    int winner = t4.IsWinner(gameBoard);
-    if (winner == 1)
-    {
-        Console.WriteLine("Player 1 won!");
-        endgame = false;
-    }
-    else if (winner == 2)
-    {
-        Console.WriteLine("Player 2 won!");
-        endgame = false;
-    }
-    else if (winner == -1)
-    {
-        Console.WriteLine("CATS GAME!");
-        endgame = false;
-    }
-    else { continue; }
+} while (playagain);
     
-}
 // print board method
 t4.PrintBoard(gameBoard);
