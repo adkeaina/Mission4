47d55959c7a9263012b204156277c6b199766768
nicktimbellini <nicholas.bellini@live.com>
2025-01-27 12:51:12 -0700

    Added intro to game

--- Program.cs
+++ Program.cs
@@ -2,5 +2,18 @@
 
 using Mission4;
 
-Console.WriteLine("Hello, World!");
+// Introduction and instantiating supporting tools
+Console.WriteLine("Hello, welcome to Tic-Tac-Toe!");
 TicTacToeTools t4 = new TicTacToeTools();
+
+// Initializing gameboard
+char[,] GameBoard = new char[3, 3];
+
+int player = 1;
+int player = 2;
+
+Console.WriteLine("Player 1 (X) Enter your choice: ";
+
+Console.WriteLine("Player 1 (O) Enter your choice: ";
+
+// print board method
